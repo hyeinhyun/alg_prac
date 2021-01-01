a55221c17f117e114eed7b1b73dec7c4566e3f5a
현혜인 <hyunhi1123@gmail.com>
2021-01-01 22:06:28 +0900

    20210101 2014

--- 2014.py
+++ 2014.py
@@ -7,19 +7,18 @@ Created on Wed Dec 23 13:23:05 2020
 """
 from heapq import heappush,heappop
 def solution(N,arr):
-    q1=arr
-    q2=arr.copy()
+    q1=[]
+    for i in arr:
+        heappush(q1,(i,i))#1이 곱해졋다고 가정
     count=0
-    val1=0
-    while count!=N+1:
+    while count<N:
         val1=heappop(q1)
-
-        for x in q2:#2,3,5,7
-            if x<=val1:
-                print(x*val1)
-                heappush(q1,x*val1)
+        print(q1)
+        for x in arr:#2,3,5,7
+            if val1[1]>=x:
+                heappush(q1,(x*val1[0],x))
         count+=1
-    return val1
+    return val1[0]
         
 #print(solution(19,[2,3,5,7]))
 L,N=map(int,input().split(' '))
