c084d9f18230b541257c37dc98052bd56cd3c754
현혜인 <hyunhi1123@gmail.com>
2021-01-03 19:32:05 +0900

    0103

--- boj/1715.py
+++ boj/1715.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  6 14:48:57 2020
+Created on Sun Jan  3 18:43:51 2021
 
 @author: hihyun
 """
-
 from heapq import heappush,heappop
+def solution(card):
+    init=0
+    answer=0
+    while card:
+        x=heappop(card)
+        if init!=0:
+            heappush(card,init+x)
+            answer+=init+x
+            init=0
+        else:
+            init=x
+    return answer-sum(card)
+            
 
-a=[]
-num=int(input())
-answer=0
-for i in range(num):
-    heappush(a,int(input()))
-if num<2:
-    print(heappop(a) if a else 0)
-else:
-    answer=heappop(a)+heappop(a)
-    for i in range(num-2):
-            print(a)
 
-            answer+=answer+(heappop(a) if a else 0)
-            print(answer)
-    print(answer)
+N=int(input())
+card=[]
+for i in range(N):
+    heappush(card,int(input()))
+print(solution(card))
