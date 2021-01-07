72399b9e1781a13c5196de91f1a411dc127bd9f8
현혜인 <hyunhi1123@gmail.com>
2021-01-07 16:53:44 +0900

    0107

--- kakao기출/[kakao2020]자물쇠.py
+++ kakao기출/[kakao2020]자물쇠.py
@@ -1,34 +1,40 @@
+import copy
 def rotation(key):
-    M=len(key)
-    keys=[]
-    keys.append(key)
+    keys=[key]
     for _ in range(3):
-        temp=[[0]*M for i in range(M)]
-
-        for i in range(M):
-            for j in range(M):
-                temp[j][M-i-1]=key[i][j]
+        temp=list(zip(*key[::-1]))
         keys.append(temp)
+
         key=temp
     return keys
+def check(result):
+    N=len(result)
+    w=[[1]*N for i in range(N)]
+    if result == w:
+        return True
+    return False
 def solution(key, lock):
     keys=rotation(key)
     N=len(lock)
-    M=len(key)
-    answer = True
+    M=len(key)-1
+    answer = False
+    for k in keys:
+        for i in range(N+M+1):
+            for j in range(N+M+1):
+                print((i,j))
+                lock_t=copy.deepcopy(lock)
+                #겹치는 부분 변경 : 범위 : [i,i-1,...i-M][j,j-1,...,j-M]
+                for idx_m,i_m in enumerate(range(i-M,i+1)):
+                    if i_m>=0 and i_m<N:#do something
+                        for jdx_m,j_m in enumerate(range(j-M,j+1)):
+                            if j_m>=0 and j_m<N:
+                                print(i_m,j_m)
+                                lock_t[i_m][j_m]=lock[i_m][j_m]+k[idx_m][jdx_m]
+                if check(lock_t):
+                    return True
 
-    for i in range(N):
-        for j in range(N):
-            #범위 (i,j)
-            if i-M+1>=0 and j-M+1>=0:
-                #범위 : i-M+1~j-M+1
-                for k in keys:
-                    for ii in range(i-M+1,i+1):
-                        for jj in range(j-M+1,i+1):
-                            if lock[ii][jj]==1 and key[ii][jj]==1:
-                                break
-                            if lock[ii][jj]==0 and key[ii][jj]==0:
-                                break
+
                 
                 
     return answer
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],[[1, 1, 1], [1, 1, 0], [1, 0, 1]]	))
